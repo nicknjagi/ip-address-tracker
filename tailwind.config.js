166995726e5c5a0452f65122184975d8fbe3a7a3
nicknjagi <njaginicholas@gmail.com>
2023-08-15 20:20:20 +0300

    add custom variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "VeryDarkGray": "hsl(0, 0%, 17%)",
+        "DarkGray": "hsl(0, 0%, 59%)",
+      },
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
